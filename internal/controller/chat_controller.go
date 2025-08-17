0fac0d64bfb00add69291dd108e23489ae06351e
Natouch Srikiatden <natouchf40@gmail.com>
2025-08-17 22:26:12 +0700

    feat:api socket

--- internal/controller/chat_controller.go
+++ internal/controller/chat_controller.go
@@ -14,21 +14,19 @@ func MessageHandler(conn *websocket.Conn, msg service.MessageInput) {
 	msg.IPAddress = "" // optional: extract from request
 
 	// Save user message & get bot response
-	savedUserMsg, botMsg, err := service.GotNewMessage(msg)
+	_, botMsg, err := service.GotNewMessage(msg)
 	if err != nil {
 		log.Printf("Error saving message: %v", err)
 		conn.WriteJSON(map[string]string{"error": "Failed to save message"})
 		return
 	}
 
-	// Send user message back to client
-	if err := conn.WriteJSON(savedUserMsg); err != nil {
-		log.Printf("Error sending user message: %v", err)
-		return
-	}
-
 	// Send bot message back to client
-	if err := conn.WriteJSON(botMsg.Message); err != nil {
+	if err := conn.WriteJSON(map[string]string{
+		"message": botMsg.Message,
+		"uid":     botMsg.UID,
+		"sender":  botMsg.Sender,
+	}); err != nil {
 		log.Printf("Error sending bot message: %v", err)
 		return
 	}
